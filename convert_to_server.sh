0886f4c84d168e42945dd4c0bbb1e3627fa1ce1e
black.dragon74 <nickk.2974@gmail.com>
2020-06-20 00:14:40 +0530

    Abort on failures
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- convert_to_server.sh
+++ convert_to_server.sh
@@ -3,26 +3,31 @@
 echo "** Script by Nick aka black.dragon74 **"
 echo "Converting Desktop to server..."
 
+function abort() {
+    echo "Fatal error: ${1}"
+    exit 1
+}
+
 # update
-sudo apt-get update
+sudo apt-get update || abort "Unable to update"
 
 # install the 'tasksel' package so we can remove the desktop image
-sudo apt-get install tasksel
+sudo apt-get install tasksel || abort "Unable to install tasksel"
 
 # remove the desktop image
-sudo tasksel remove ubuntu-desktop
+sudo tasksel remove ubuntu-desktop || abort "Unable to remove desktop image"
 
 # tell tasksel to start the server image setup
-sudo tasksel install server
+sudo tasksel install server || abort "Unable to tell tasksel to install server image"
 
 # install the server images
-sudo apt-get install linux-server linux-image-server
+sudo apt-get install linux-server linux-image-server || abort "Unable to install server image"
 
 # remove lightdm
-sudo apt-get purge lightdm
+sudo apt-get purge lightdm || abort "Unable to remove lightdm"
 
 # remove all packages no longer required (~400 MB)
-sudo apt-get autoremove
+sudo apt-get autoremove || abort "Unable to autoremove obsolete packages"
 
 # we are done
 echo "We are all done.. Enjoy!"
